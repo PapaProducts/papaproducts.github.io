262623f4fe0bbcad83b4ab9d4f92ac1754a2f547
Neelansh Sahai <neelansh.sahai@shipsy.co.in>
2020-11-28 22:27:23 +0530

    Fix 1.0.0 Build, added next.config.js

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a68feaf3317918b65df6";
+/******/ 	var hotCurrentHash = "8b877fb5faf5bef36abc";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
